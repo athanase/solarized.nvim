1e567736a210c1608f3ec664d644f14e58ce74d0
Jean Inderchit <jean.inderchit@gmail.com>
2023-01-24 21:49:44 +0100

    Tweaking colors

--- lua/lualine/themes/solarized.lua
+++ lua/lualine/themes/solarized.lua
@@ -1,37 +1,37 @@
-local colors = require('solarized.colors')
+local colors = require("solarized.colors")
 
 local solarized = {}
 
 solarized.normal = {
-	a = {fg = colors.bg, bg = colors.accent, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
-	c = {fg = colors.fg, bg = colors.selection},
+	a = { fg = colors.bg, bg = colors.blue, gui = "bold" },
+	b = { fg = colors.title, bg = colors.selection },
+	c = { fg = colors.fg, bg = colors.selection },
 }
 
 solarized.insert = {
-	a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	a = { fg = colors.bg, bg = colors.green, gui = "bold" },
+	b = { fg = colors.title, bg = colors.selection },
 }
 
 solarized.visual = {
-	a = {fg = colors.bg, bg = colors.purple, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	a = { fg = colors.bg, bg = colors.violet, gui = "bold" },
+	b = { fg = colors.title, bg = colors.selection },
 }
 
 solarized.replace = {
-	a = {fg = colors.bg, bg = colors.red, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	a = { fg = colors.bg, bg = colors.magenta, gui = "bold" },
+	b = { fg = colors.title, bg = colors.selection },
 }
 
 solarized.command = {
-	a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	a = { fg = colors.bg, bg = colors.cyan, gui = "bold" },
+	b = { fg = colors.title, bg = colors.selection },
 }
 
 solarized.inactive = {
-  a = {fg = colors.disabled, bg = colors.bg, gui = 'bold'},
-  b = {fg = colors.disabled, bg = colors.bg},
-  c = {fg = colors.disabled, bg = colors.selection}
+	a = { fg = colors.none, bg = colors.selection, gui = "bold" },
+	b = { fg = colors.none, bg = colors.selection },
+	c = { fg = colors.none, bg = colors.selection },
 }
 
 return solarized
--- lua/solarized/colors.lua
+++ lua/solarized/colors.lua
@@ -1,75 +1,48 @@
 local bases = {
-	base03 =        '#002b36',
-	base02 =        '#073642',
-	base01 =        '#586e75',
-	base00 =        '#657b83',
-	base0 =         '#839496',
-	base1 =         '#93a1a1',
-	base2 =         '#eee8d5',
-	base3 =         '#fdf6e3',
+	base03 = "#002b36",
+	base02 = "#073642",
+	base01 = "#586e75",
+	base00 = "#657b83",
+	base0 = "#839496",
+	base1 = "#93a1a1",
+	base2 = "#eee8d5",
+	base3 = "#fdf6e3",
 }
 
 local solarized = {
+	base03 = "#002b36",
+	base02 = "#073642",
+	base01 = "#586e75",
+	base00 = "#657b83",
+	base0 = "#839496",
+	base1 = "#93a1a1",
+	base2 = "#eee8d5",
+	base3 = "#fdf6e3",
 
-	white =         '#eee8d5',
-	gray =          '#073642',
-	black =         '#002b36',
-	red =           '#dc322f',
-	green =         '#7d8d09',
-	yellow =        '#6c71c4',
-	paleblue =      '#586e75',
-	cyan =          '#7d8d09',
-	blue =          '#073642',
-	purple =        '#b58900', -- why is this and purple swapped??
-	orange =        '#cb4b16',
-	pink =          '#073642',
+	yellow = "#b58900",
+	orange = "#cb4b16",
+	red = "#dc322f",
+	magenta = "#d33682",
+	violet = "#6c71c4",
+	blue = "#268bd2",
+	cyan = "#2aa198",
+	green = "#859900",
 
-	bg_light =       bases['base3'],
-	bg_light_alt =   bases['base2'],
-	bg_dark =        bases['base03'],
-	bg_dark_alt =    bases['base02'],
-	fg_light =       bases['base0'],
-	text_light =     bases['base1'],
-	fg_dark =        bases['base00'],
-	text_dark =      bases['base01'],
-	comments =      '#657b83',
-	selection =     '#d3cfc1',
-	contrast =      '#002b36',
-	active =        '#d8ccc4',
-	border =        '#002b36',
-	line_numbers =  '#839496',
-	highlight =     '#d8ccc4',
-	disabled =      '#073642',
-	cursor =        '#268bd2',
-	accent =        '#073642',
+	fg = bases["base00"],
+	bg = bases["base03"],
+	comments = bases["base01"],
+	selection = bases["base02"],
+	border = "#002b36",
+	line_numbers = bases["base01"],
+	highlight = "#d8ccc4",
+	disabled = "#073642",
+	cursor = "#268bd2",
+	accent = "#a73642",
 
-	error =         '#d33682',
-	link =          '#2aa198',
+	error = "#d33682",
+	link = "#2aa198",
 
-	none =          'NONE'
+	none = "NONE",
 }
-
--- If dark mode, swap fg and bg
-if vim.o.background == 'dark' then
-	solarized.bg = solarized.bg_dark
-	solarized.bg_alt = solarized.bg_dark_alt
-	solarized.fg = solarized.fg_dark
-	solarized.text = solarized.text_dark
-else
-	solarized.bg = solarized.bg_light
-	solarized.bg_alt = solarized.bg_light_alt
-	solarized.fg = solarized.fg_light
-	solarized.text = solarized.text_light
-end
--- Optional colors
-
--- Enable contrast sidebars, floating windows and popup menus
-if vim.g.solarized_contrast == false then
-    solarized.sidebar = solarized.bg
-    solarized.float = solarized.bg
-else
-    solarized.sidebar = solarized.bg_alt
-    solarized.float = solarized.bg_alt
-end
 
 return solarized
